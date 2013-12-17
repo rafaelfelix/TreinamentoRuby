d0c46008a3c4ed41d81785d0fa4c9034e63dab98
Rafael Felix Correa <rafael.felix@gmail.com>
2013-12-17 16:07:21 -0200

    problema de encoding com acentos no 1.9.3

--- modo.rb
+++
@@ -1,8 +0,0 @@
-class Modo
-    attr_accessor :Nome
-
-    def initialize(p1,p2,p3)
-    end
-end
-
-#class Modo; def initialize(p1); @p1 = p1; end; def p1; @p1; end; def Nome; @nome; end; def Nome=(nome); @nome = nome; end; end;
--- primeiro.rb
+++ primeiro.rb
@@ -1,5 +1,24 @@
-def Fixnum.calcula
-    "Felix"
+# -*- encoding : utf-8 -*-
+class Modo
+    def initialize(p1)
+        @p1 = p1
+    end
+    
+    def p1
+        @p1
+    end
+    
+    def stat
+        "ação"
+    end
+
+    def Nome
+        @nome
+    end
+    
+    def Nome=(nome)
+        @nome = nome
+    end
 end
 
-puts "Rafael " + Fixnum.calcula;
+puts 'Mister ' + Modo.new('felicão').p1
